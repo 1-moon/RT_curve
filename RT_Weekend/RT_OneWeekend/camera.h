19e9d1df5cc5b561f260c7ca144b409d5e9eed56
1-moon <hhm9124@gmail.com>
2024-03-26 20:15:19 +0000

    move camera renderting part in main.cpp into camera.h

--- RT_Weekend/RT_OneWeekend/camera.h
+++ RT_Weekend/RT_OneWeekend/camera.h
@@ -4,78 +4,101 @@
 #include "color.h"
 #include "hittable.h"
 #include "utility.h"
+#include "material.h"
 #include <iostream>
 
 class Camera {
-	public: 
-		double aspect_ratio = 1.0;	// Ratio of image width over height 
-		int image_width = 100;		// Rendered image width in pixel count 
-		
-		Camera() {
-			double viewport_height = 2.0;
-			double viewport_width = aspect_ratio * viewport_height;	//3.555
-			double focal_length = 1.0;
+public:
+	// Image data 
+	double aspect_ratio;
+	int image_width;
 
-			point3 origin = point3(0, 0, 0);
-			Vec3 lower_left_corner = origin - Vec3(viewport_width / 2, viewport_height / 2, focal_length);
-			Vec3 horizontal = Vec3(viewport_width, 0.0, 0.0);
-			Vec3 vertical = Vec3(0.0, viewport_height, 0.0);
-		}
 
-		
-		void render(const Hittable& world) {
-			initialize();
-			  
-			std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+	void render(const Hittable_list& world) {
+		init();
+		std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-			for (int j =0; j < image_height; ++j) {
-				std::cerr << "Scanlines remaining: " << (image_height -j) << '\n';
-				for (int i = 0; i < image_width; ++i) {
-					auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-					auto ray_direction = pixel_center - camera_from;
-					Ray r(camera_from, ray_direction);
+		for (int j = image_height - 1; j >= 0; j--) {
+			std::cerr << "Scanlines remaining: " << j << '\n';
+			for (int i = 0; i < image_width; i++) {
+				color pixel_color(0, 0, 0);
+				// Apply anti-aliasing method   
+				for (int sample_ray = 0; sample_ray < sample_rays_per_pixel; ++sample_ray) {
+					// Generate random value [0, 1) 
+					double random_u = static_cast<double>(rand()) / (RAND_MAX + 1.0);
+					double random_v = static_cast<double>(rand()) / (RAND_MAX + 1.0);
 
-					color pixel_color = ray_color(r, world);
-					write_color(std::cout, pixel_color,10);
+					// scalars that vary for each pixel to iterate over the entire plane.
+					auto u = (i + random_u) / (image_width - 1);	// 0~1
+					auto v = (j + random_v) / (image_height - 1);	// 0~1
+					// Create a ray from the camera origin to the pixel on the virtual viewport
+					Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+					pixel_color += ray_color(r, world,10);
 				}
+				write_color(std::cout, pixel_color, sample_rays_per_pixel);
 			}
-			std::cerr << "\nDone.\n";
+		}
+		std::cerr << "\nDone.\n";
+	}
+
+
+private:
+	int image_height;
+	//virtual viewport
+	double viewport_height;
+	double viewport_width;
+	double focal_length;
+	point3 origin ;
+	Vec3 lower_left_corner;
+	Vec3 horizontal;
+	Vec3 vertical ;
+	// ray data
+	int sample_rays_per_pixel = 10;
+
+	void init() {
+		image_height = static_cast<int>(image_width / aspect_ratio);	// Estimate around 225px
+
+		// Set viewport size 
+		viewport_height = 2.0;  
+		viewport_width = aspect_ratio * viewport_height;
+
+		// Set camera data 
+		focal_length = 1.0; 
+		origin = point3(0, 0, 0); 
+		lower_left_corner = origin - Vec3(viewport_width / 2, viewport_height / 2, focal_length);
+
+		// 뷰포트를 따라 가로와 세로로 이동하는 벡터 계산
+		horizontal = Vec3(viewport_width, 0.0, 0.0);
+		vertical = Vec3(0.0, viewport_height, 0.0);
+	}
+
+	color ray_color(const Ray& r, const Hittable_list world, int depth) {
+
+		Hit_record record;
+		// kick things off by setting the initial range 
+		if (world.TestIntersection(r, Interval(0.0001, infinity), record)) {
+			/*
+			//Diffuse reflection
+			Vec3 random_reflect_ray = rec.int_p + rec.normal + random_unit_vector();
+			return 0.5 * ray_color(Ray(rec.int_p, random_reflect_ray - rec.int_p), world); // // Set the attenuation factor to 0.5 for a 50% reduction per bounce
+			*/
+
+			// Reflection - material scatter
+			Ray scattered;
+			color attenuation;
+			if (record.mat_ptr->scatter(r, record, attenuation, scattered))
+				return attenuation * ray_color(scattered, world, depth - 1);
+			return color(0, 0, 0);
 
 		}
-
-		Ray get_ray_from_pixel(double u, double v) {
-			return Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-		}
-		point3 origin;
-		Vec3 lower_left_corner;
-		Vec3 horizontal;
-		Vec3 vertical;
-	private: 
-		int image_height;		// Rendered image height 
-		point3 camera_from;		// Camera location
-		point3 pixel00_loc;		// Location of the top-left pixel (0,0)
-		Vec3 pixel_delta_u;		// offset to pixel to the right
-		Vec3 pixel_delta_v ;	// offset to pixel below
-
-
-
-		void initialize() {
-		}
-
-
-		color ray_color(const Ray& r, const Hittable& world) const {
-			Hit_record rec;
-			
-			if (world.TestIntersection(r, interval(0, infinity), rec)) {
-				return 0.5 * (rec.normal + color(1, 1, 1));
-			}
-
+		else {
+			// Return background color 
 			Vec3 unit_direction = Normalize(r.direction());
-			auto t = 0.5 * (unit_direction.y() + 1.0);
+			auto t = 0.5 * (unit_direction.y() + 1.0);	 // add 1 to ensure it's above zero, multiply by 0.5 to get a num between 0~1
 			return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 		}
 
+	}
 };
-
 #endif
 
--- RT_Weekend/RT_OneWeekend/main.cpp
+++ RT_Weekend/RT_OneWeekend/main.cpp
@@ -4,46 +4,15 @@
 #include "utility.h" // Vec(point, color), Ray, interval(infinity) 
 #include "color.h"	// write_color
 #include "hittable_list.h"  
-//#include "camera.h"
+#include "camera.h"
 #include "material.h"	// Lambertian
 // Import primitive files   
 #include "bezierCurve.h"
 #include "sphere.h"
 
-color ray_color(const Ray& r, const Hittable_list world, int depth) {
-
-	Hit_record record;
-	// kick things off by setting the initial range 
-	if (world.TestIntersection(r, Interval(0.0001, infinity), record)) {
-		/* 
-		//Diffuse reflection
-		Vec3 random_reflect_ray = rec.int_p + rec.normal + random_unit_vector();
-		return 0.5 * ray_color(Ray(rec.int_p, random_reflect_ray - rec.int_p), world); // // Set the attenuation factor to 0.5 for a 50% reduction per bounce
-		*/
-		
-		// Reflection - material scatter
-		Ray scattered;
-		color attenuation;
-		if (record.mat_ptr->scatter(r,record, attenuation, scattered))
-			return attenuation * ray_color(scattered, world, depth - 1);
-		return color(0, 0, 0);
-		
-	}
-	else {
-		// Return background color 
-		Vec3 unit_direction = Normalize(r.direction());
-		auto t = 0.5 * (unit_direction.y() + 1.0);	 // add 1 to ensure it's above zero, multiply by 0.5 to get a num between 0~1
-		return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-	}
-
-}
 
 int main()
 {
-	// Image data 
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
-	const int image_height = static_cast<int>(image_width / aspect_ratio);	// 225px 
 
 	// Material data 
 	auto Material_lambertian = make_shared<Lambertian>( color(0.8, 0.1, 0.3) ); 
@@ -61,40 +30,13 @@ int main()
 	world.add(make_shared<Sphere>(point3(1, 0, -1), 0.5, Material_metal));
 	world.add(make_shared<Sphere>(point3(-1, 0, -1), 0.5, Material_metal));
 
-	// ========== Camera part ===============
-	// virtual viewport
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;	//3.555
-	auto focal_length = 1.0;
+	Camera camera;
+	// Set image data
+	camera.aspect_ratio = 16.0 / 9.0;
+	camera.image_width = 400;
 
-	auto origin = point3(0, 0, 0);
-	auto lower_left_corner = origin - Vec3(viewport_width / 2, viewport_height / 2, focal_length);
-	auto horizontal = Vec3(viewport_width, 0.0, 0.0);
-	auto vertical = Vec3(0.0, viewport_height, 0.0);
+	// Set camera data
+	camera.render(world);
 
-	// ========== Render part ===============
-	int sample_rays_per_pixel = 10;
-	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-	for (int j = image_height - 1; j >= 0; j--) {
-		std::cerr << "Scanlines remaining: " << j << '\n';
-		for (int i = 0; i < image_width; i++) {
-			color pixel_color(0, 0, 0);
-			// Apply anti-aliasing method   
-			for (int sample_ray = 0; sample_ray < sample_rays_per_pixel; ++sample_ray) {
-				// Generate random value [0, 1) 
-				double random_u = static_cast<double>(rand()) / (RAND_MAX + 1.0);
-				double random_v = static_cast<double>(rand()) / (RAND_MAX + 1.0);
-
-				// scalars that vary for each pixel to iterate over the entire plane.
-				auto u = (i + random_u) / (image_width - 1);	// 0~1
-				auto v = (j + random_v) / (image_height - 1);	// 0~1
-				// Create a ray from the camera origin to the pixel on the virtual viewport
-				Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-				pixel_color += ray_color(r, world,10);
-			}
-			write_color(std::cout, pixel_color, sample_rays_per_pixel);
-		}
-	}
-	std::cerr << "\nDone.\n";
 }
