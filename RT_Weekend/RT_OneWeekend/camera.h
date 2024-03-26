814f73b7d00ad5bc436f1fdc442667a74daaad04
1-moon <hhm9124@gmail.com>
2024-03-26 23:19:56 +0000

    add fov in camera.h

--- RT_Weekend/RT_OneWeekend/camera.h
+++ RT_Weekend/RT_OneWeekend/camera.h
@@ -7,19 +7,24 @@
 #include "material.h"
 #include <iostream>
 
+
 class Camera {
 public:
-	// Image data 
-	double aspect_ratio;
-	int image_width;
-
-
+	// image data 
+	double aspect_ratio=0.0;
+	int image_width= 0;
+	// camera frame  
+	double vfov = 90;
+	point3 lookFrom = point3(-2,2,1);
+	point3 lookAt = point3(0,0,-1);
+	Vec3 up_vec = Vec3(0, 1, 0);
+	
 	void render(const Hittable_list& world) {
 		init();
 		std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-		for (int j = image_height - 1; j >= 0; j--) {
-			std::cerr << "Scanlines remaining: " << j << '\n';
+		for (int j = 0; j < image_height; j++) {
+			std::cerr << "Scanlines remaining: " << (image_height - j)<< '\n';
 			for (int i = 0; i < image_width; i++) {
 				color pixel_color(0, 0, 0);
 				// Apply anti-aliasing method   
@@ -45,31 +50,45 @@ public:
 private:
 	int image_height;
 	//virtual viewport
-	double viewport_height;
-	double viewport_width;
-	double focal_length;
+	//double viewport_height;
+	//double viewport_width;
+	//double focal_length;
 	point3 origin ;
 	Vec3 lower_left_corner;
 	Vec3 horizontal;
 	Vec3 vertical ;
 	// ray data
 	int sample_rays_per_pixel = 10;
+	// camera coordinates 
+	Vec3 frame_u, frame_v, frame_w;// camera frmae unit vectors  
 
 	void init() {
 		image_height = static_cast<int>(image_width / aspect_ratio);	// Estimate around 225px
-
+		//focal_length = 1.0;
 		// Set viewport size 
-		viewport_height = 2.0;  
-		viewport_width = aspect_ratio * viewport_height;
-
+		//viewport_height = 2.0;  
+		//viewport_width = aspect_ratio * viewport_height;
+		
+		// Determin viewport dimenstions 
+		auto theta = vfov * (pi / 180);
+		auto half_h = tan(theta / 2);
+		auto half_w = aspect_ratio * half_h;
 		// Set camera data 
-		focal_length = 1.0; 
-		origin = point3(0, 0, 0); 
-		lower_left_corner = origin - Vec3(viewport_width / 2, viewport_height / 2, focal_length);
+		origin = lookFrom;
+		// Calculate the basis vectors for the camera coordinate frame.  
+		frame_w = Normalize(lookFrom - lookAt);
+		frame_u = Normalize(cross(up_vec, frame_w));
+		frame_v = cross(frame_u, frame_w);
+
 
 		// 뷰포트를 따라 가로와 세로로 이동하는 벡터 계산
-		horizontal = Vec3(viewport_width, 0.0, 0.0);
-		vertical = Vec3(0.0, viewport_height, 0.0);
+		//lower_left_corner = origin - Vec3(viewport_width / 2, viewport_height / 2, focal_length);
+		//horizontal = Vec3(viewport_width, 0.0, 0.0);
+		//vertical = Vec3(0.0, viewport_height, 0.0);
+		lower_left_corner = origin - half_w*frame_u - half_h*frame_v - frame_w;
+		horizontal = 2* half_w * frame_u;
+		vertical = 2* half_h * frame_v;
+
 	}
 
 	color ray_color(const Ray& r, const Hittable_list world, int depth) {
--- RT_Weekend/RT_OneWeekend/utility.h
+++ RT_Weekend/RT_OneWeekend/utility.h
@@ -12,6 +12,7 @@ using std::numeric_limits;
 using std::sqrt;
 // constants
 const double infinity = numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
 
 // Utility Functions
 inline double random_double() {
